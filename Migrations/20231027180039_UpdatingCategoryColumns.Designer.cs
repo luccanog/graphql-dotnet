191b53305843a7ebdaa9c8ae249ea813581e955d
luccanog <luccathomaz@gmail.com>
2023-10-27 15:01:22 -0300

    updating database's Category table

--- Migrations/20231027180039_UpdatingCategoryColumns.Designer.cs
+++ Migrations/20231027180039_UpdatingCategoryColumns.Designer.cs
@@ -4,6 +4,7 @@ using GraphQL.Dotnet.Entities.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GraphQL.Dotnet.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231027180039_UpdatingCategoryColumns")]
+    partial class UpdatingCategoryColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20231027180039_UpdatingCategoryColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQL.Dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingCategoryColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Categories",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "ImagemUrl",
+                table: "Categories",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Categories",
+                newName: "ImagemUrl");
+        }
+    }
+}
