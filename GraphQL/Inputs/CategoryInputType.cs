2240e6bc3f3c1b44ce90ef3722b4768793c42a1e
luccanog <luccathomaz@gmail.com>
2023-10-28 12:25:52 -0300

    removing DTO and creating graphql InputType

---
+++ GraphQL/Inputs/CategoryInputType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Dotnet.Entities;
+using GraphQL.Types;
+
+namespace GraphQL.Dotnet.GraphQL.Inputs
+{
+    public class CategoryInputType : InputObjectGraphType
+    {
+        public CategoryInputType()
+        {
+            Name = "CategoryInput";
+            Field<NonNullGraphType<StringGraphType>>(nameof(Category.Name));
+            Field<NonNullGraphType<StringGraphType>>(nameof(Category.ImageUrl));
+        }
+    }
+}
